f7b0612b5fb9076ca2534fb68615d38ce5ec3c34
crccheck <c@crccheck.com>
2013-06-20 02:51:00 +0000

    move tbody code out

--- lazyColor/js/main.js
+++ lazyColor/js/main.js
@@ -4,7 +4,7 @@ require.config({
 });
 
 
-require(['utils'], function(utils) {
+require(['utils', 'tbody'], function(utils, tbody) {
 
 /*global $, Color */
 var w3ccolors = [["aliceblue","#f0f8ff"],["antiquewhite","#faebd7"],["aqua","#00ffff"],["aquamarine","#7fffd4"],["azure","#f0ffff"],["beige","#f5f5dc"],["bisque","#ffe4c4"],["black","#000000"],["blanchedalmond","#ffebcd"],["blue","#0000ff"],["blueviolet","#8a2be2"],["brown","#a52a2a"],["burlywood","#deb887"],["cadetblue","#5f9ea0"],["chartreuse","#7fff00"],["chocolate","#d2691e"],["coral","#ff7f50"],["cornflowerblue","#6495ed"],["cornsilk","#fff8dc"],["crimson","#dc143c"],["cyan","#00ffff"],["darkblue","#00008b"],["darkcyan","#008b8b"],["darkgoldenrod","#b8860b"],["darkgray","#a9a9a9"],["darkgreen","#006400"],["darkgrey","#a9a9a9"],["darkkhaki","#bdb76b"],["darkmagenta","#8b008b"],["darkolivegreen","#556b2f"],["darkorange","#ff8c00"],["darkorchid","#9932cc"],["darkred","#8b0000"],["darksalmon","#e9967a"],["darkseagreen","#8fbc8f"],["darkslateblue","#483d8b"],["darkslategray","#2f4f4f"],["darkslategrey","#2f4f4f"],["darkturquoise","#00ced1"],["darkviolet","#9400d3"],["deeppink","#ff1493"],["deepskyblue","#00bfff"],["dimgray","#696969"],["dimgrey","#696969"],["dodgerblue","#1e90ff"],["firebrick","#b22222"],["floralwhite","#fffaf0"],["forestgreen","#228b22"],["fuchsia","#ff00ff"],["gainsboro","#dcdcdc"],["ghostwhite","#f8f8ff"],["gold","#ffd700"],["goldenrod","#daa520"],["gray","#808080"],["green","#008000"],["greenyellow","#adff2f"],["grey","#808080"],["honeydew","#f0fff0"],["hotpink","#ff69b4"],["indianred","#cd5c5c"],["indigo","#4b0082"],["ivory","#fffff0"],["khaki","#f0e68c"],["lavender","#e6e6fa"],["lavenderblush","#fff0f5"],["lawngreen","#7cfc00"],["lemonchiffon","#fffacd"],["lightblue","#add8e6"],["lightcoral","#f08080"],["lightcyan","#e0ffff"],["lightgoldenrodyellow","#fafad2"],["lightgray","#d3d3d3"],["lightgreen","#90ee90"],["lightgrey","#d3d3d3"],["lightpink","#ffb6c1"],["lightsalmon","#ffa07a"],["lightseagreen","#20b2aa"],["lightskyblue","#87cefa"],["lightslategray","#778899"],["lightslategrey","#778899"],["lightsteelblue","#b0c4de"],["lightyellow","#ffffe0"],["lime","#00ff00"],["limegreen","#32cd32"],["linen","#faf0e6"],["magenta","#ff00ff"],["maroon","#800000"],["mediumaquamarine","#66cdaa"],["mediumblue","#0000cd"],["mediumorchid","#ba55d3"],["mediumpurple","#9370db"],["mediumseagreen","#3cb371"],["mediumslateblue","#7b68ee"],["mediumspringgreen","#00fa9a"],["mediumturquoise","#48d1cc"],["mediumvioletred","#c71585"],["midnightblue","#191970"],["mintcream","#f5fffa"],["mistyrose","#ffe4e1"],["moccasin","#ffe4b5"],["navajowhite","#ffdead"],["navy","#000080"],["oldlace","#fdf5e6"],["olive","#808000"],["olivedrab","#6b8e23"],["orange","#ffa500"],["orangered","#ff4500"],["orchid","#da70d6"],["palegoldenrod","#eee8aa"],["palegreen","#98fb98"],["paleturquoise","#afeeee"],["palevioletred","#db7093"],["papayawhip","#ffefd5"],["peachpuff","#ffdab9"],["peru","#cd853f"],["pink","#ffc0cb"],["plum","#dda0dd"],["powderblue","#b0e0e6"],["purple","#800080"],["red","#ff0000"],["rosybrown","#bc8f8f"],["royalblue","#4169e1"],["saddlebrown","#8b4513"],["salmon","#fa8072"],["sandybrown","#f4a460"],["seagreen","#2e8b57"],["seashell","#fff5ee"],["sienna","#a0522d"],["silver","#c0c0c0"],["skyblue","#87ceeb"],["slateblue","#6a5acd"],["slategray","#708090"],["slategrey","#708090"],["snow","#fffafa"],["springgreen","#00ff7f"],["steelblue","#4682b4"],["tan","#d2b48c"],["teal","#008080"],["thistle","#d8bfd8"],["tomato","#ff6347"],["turquoise","#40e0d0"],["violet","#ee82ee"],["wheat","#f5deb3"],["white","#ffffff"],["whitesmoke","#f5f5f5"],["yellow","#ffff00"],["yellowgreen","#9acd32"]];
@@ -37,7 +37,7 @@ var ENABLE_HISTORY = location.protocol.substr(0,4) == 'http' &&
     if (!state || !state.color){ return; }
     $input.val(state.color);
     $('.input-label').text(state.color);
-    lazyColor.newColor(state.color, true);
+    tbody.newColor(state.color, true);
   });
 
   // autopopulate input if color found in url
@@ -57,137 +57,6 @@ var ENABLE_HISTORY = location.protocol.substr(0,4) == 'http' &&
   // exports
   exports.appHistory = appHistory;
 })(window);
-
-
-// the code to generate the tbody
-/*global $tbody, Color, d3 */
-var lazyColor = (function(){
-  "use strict";
-
-  var data,
-      lastD,
-      $first,
-      $el = $tbody,
-      paper = d3.select($el[0]),
-      rows = paper.selectAll('tr'),
-      weights = {
-        l: 1,
-        a: 1,
-        b: 1,
-        h: 0,
-        s: 0,
-        v: 0
-      };
-
-  var distance = function(c1, c2) {
-    return Math.abs(c1.l - c2.l) +
-      Math.abs(c1.a - c2.a) +
-      Math.abs(c1.b - c2.b) +
-      weights.h * Math.abs(c1.h - c2.h) +
-      weights.s * Math.abs(c1.s - c2.s) +
-      weights.v * Math.abs(c1.v - c2.v);
-  };
-
-  // a template.... ish
-  var templateish = function(d){
-    var title = 'LAB: ' + d.l.toFixed(2) + ',' + d.a.toFixed(2) + ',' + d.b.toFixed(2) +
-      ' HSV: ' + d.h + ',' + d.s + ',' + d.v;
-    return '<td style="background-color: transparent;">&nbsp;</td>' +
-      '<td style="background-color: ' + d.hex + ';" title="' + title + '">' +
-      '<span class="named" style="background-color: ' + d.name + ';">&nbsp;</span>&nbsp;</td>' +
-      '<td class="name">' + d.name + '</td>' +
-      '<td class="hex">' + d.hex + '</td>' +
-      '<tr>';
-  };
-
-  // convert ['name', '#abc'] to a data point
-  var getDatum = function(value){
-    // manually extend the hash instead of using an extend helper for speed
-    var d = Color.convert(value[1].substr(1), 'lab'),
-        hsv = Color.convert(value[1].substr(1), 'hsv');
-    d.name = value[0];
-    d.hex = value[1];
-    d.h = hsv.h;
-    d.s = hsv.s;
-    d.v = hsv.v;
-    return d;
-  };
-
-  var setWeight = function(key, value) {
-    weights[key] = value;
-    // if table was already sorted once, re-sort
-    if (typeof lastD !== 'undefined') {
-      sortColorTable(lastD);
-    }
-  };
-
-  var sortColorTable = function(d) {
-    for (var i = 0, n = data.length; i < n; i++) {
-      data[i].distance = distance(d, data[i]);
-    }
-    rows.sort(function(a, b) {
-      return a.distance - b.distance;
-    });
-    // update form element
-    $input.val(d.hex);
-    $('.input-label').text(d.hex);
-
-    // HACK to get css transitions to work, need to delay setting color
-    setTimeout(function() {
-      $first.css('backgroundColor', d.hex);
-    }, 1);
-    // scroll to the top of the page
-    var $page = $('html, body');
-    if (!$page.filter(':animated').length){  // basic debounce
-      $page.animate({'scrollTop': 0});
-    }
-    lastD = d;
-  };
-
-  // render the table, replacing the tbody
-  // arguments:
-  //   colors: an array of [name, rgb]
-  var renderColorTable = function(colors) {
-    data = colors.map(getDatum);
-
-    rows = rows.data(data);
-    // CREATE
-    rows.enter()
-      .append('tr').html(templateish)
-      .on('click', sortColorTable);
-    // UPDATE
-    rows
-      .html(templateish);
-    // DELETE
-    rows.exit()
-      .remove();
-
-    $first = $el.find('tr > td:nth-child(1)');
-  };
-
-
-  // change color
-  var newColor = function(hex, inPopState) {
-    if (hex && hex == window._oldColor) {
-      return;
-    }
-    sortColorTable(getDatum(['unknown', '#' + hex]));
-    window._oldColor = hex;
-    if (ENABLE_HISTORY && inPopState !== true){
-      history.pushState({ color: hex },
-        window.appHistory.newTitle(hex),
-        window.appHistory.newPath(hex));
-    }
-  };
-
-
-  return {
-    setWeight: setWeight,
-    renderColorTable: renderColorTable,
-    newColor: newColor,
-    sort: sortColorTable
-  };
-})();
 
 
 // TOPBAR UI
@@ -212,19 +81,19 @@ var lazyColor = (function(){
   $input.on('keyup change', function(){
     var color = utils.isColor($input.val());
     if (color) {
-      lazyColor.newColor(color);
+      tbody.newColor(color);
     }
   });
 
   // bind DOM to `weights`
   $('#weight-h').on('change', function(){
-    lazyColor.setWeight('h', this.value);
+    tbody.setWeight('h', this.value);
   });
   $('#weight-s').on('change', function(){
-    lazyColor.setWeight('s', this.value);
+    tbody.setWeight('s', this.value);
   });
   $('#weight-v').on('change', function(){
-    lazyColor.setWeight('v', this.value);
+    tbody.setWeight('v', this.value);
   });
 
   $('#colors-picker .btn').click(function(){
@@ -234,14 +103,14 @@ var lazyColor = (function(){
     }
     $this.addClass(ACTIVE_CLASS).siblings().removeClass(ACTIVE_CLASS);
     // TODO don't assume it's on `window`
-    lazyColor.renderColorTable(window[$this.attr('rel')]);
+    tbody.renderColorTable(window[$this.attr('rel')]);
   });
 
 })();
 
 
 // Main
-lazyColor.renderColorTable(w3ccolors);
+tbody.renderColorTable(w3ccolors);
 
 
 });
---
+++ lazyColor/js/tbody.js
@@ -0,0 +1,134 @@
+// the code to generate the tbody
+/*global $, d3, Color */
+define(function(){
+  "use strict";
+
+  // FIXME
+  var $tbody = $('#colorTable > tbody'),
+      $input = $('#colorInput');
+
+
+  var data,
+      lastD,
+      $first,
+      $el = $tbody,
+      paper = d3.select($el[0]),
+      rows = paper.selectAll('tr'),
+      weights = {
+        l: 1,
+        a: 1,
+        b: 1,
+        h: 0,
+        s: 0,
+        v: 0
+      };
+
+  var distance = function(c1, c2) {
+    return Math.abs(c1.l - c2.l) +
+      Math.abs(c1.a - c2.a) +
+      Math.abs(c1.b - c2.b) +
+      weights.h * Math.abs(c1.h - c2.h) +
+      weights.s * Math.abs(c1.s - c2.s) +
+      weights.v * Math.abs(c1.v - c2.v);
+  };
+
+  // a template.... ish
+  var templateish = function(d){
+    var title = 'LAB: ' + d.l.toFixed(2) + ',' + d.a.toFixed(2) + ',' + d.b.toFixed(2) +
+      ' HSV: ' + d.h + ',' + d.s + ',' + d.v;
+    return '<td style="background-color: transparent;">&nbsp;</td>' +
+      '<td style="background-color: ' + d.hex + ';" title="' + title + '">' +
+      '<span class="named" style="background-color: ' + d.name + ';">&nbsp;</span>&nbsp;</td>' +
+      '<td class="name">' + d.name + '</td>' +
+      '<td class="hex">' + d.hex + '</td>';
+  };
+
+  // convert ['name', '#abc'] to a data point
+  var getDatum = function(value){
+    // manually extend the hash instead of using an extend helper for speed
+    var d = Color.convert(value[1].substr(1), 'lab'),
+        hsv = Color.convert(value[1].substr(1), 'hsv');
+    d.name = value[0];
+    d.hex = value[1];
+    d.h = hsv.h;
+    d.s = hsv.s;
+    d.v = hsv.v;
+    return d;
+  };
+
+  var setWeight = function(key, value) {
+    weights[key] = value;
+    // if table was already sorted once, re-sort
+    if (typeof lastD !== 'undefined') {
+      sortColorTable(lastD);
+    }
+  };
+
+  var sortColorTable = function(d) {
+    for (var i = 0, n = data.length; i < n; i++) {
+      data[i].distance = distance(d, data[i]);
+    }
+    rows.sort(function(a, b) {
+      return a.distance - b.distance;
+    });
+    // update form element
+    $input.val(d.hex);
+    $('.input-label').text(d.hex);
+
+    // HACK to get css transitions to work, need to delay setting color
+    setTimeout(function() {
+      $first.css('backgroundColor', d.hex);
+    }, 1);
+    // scroll to the top of the page
+    var $page = $('html, body');
+    if (!$page.filter(':animated').length){  // basic debounce
+      $page.animate({'scrollTop': 0});
+    }
+    lastD = d;
+  };
+
+  // render the table, replacing the tbody
+  // arguments:
+  //   colors: an array of [name, rgb]
+  var renderColorTable = function(colors) {
+    data = colors.map(getDatum);
+
+    rows = rows.data(data);
+    // CREATE
+    rows.enter()
+      .append('tr').html(templateish)
+      .on('click', sortColorTable);
+    // UPDATE
+    rows
+      .html(templateish);
+    // DELETE
+    rows.exit()
+      .remove();
+
+    $first = $el.find('tr > td:nth-child(1)');
+  };
+
+
+  // change color
+  var newColor = function(hex, inPopState) {
+    if (hex && hex == window._oldColor) {
+      return;
+    }
+    sortColorTable(getDatum(['unknown', '#' + hex]));
+    window._oldColor = hex;
+    // TODO
+    // if (ENABLE_HISTORY && inPopState !== true){
+    //   history.pushState({ color: hex },
+    //     window.appHistory.newTitle(hex),
+    //     window.appHistory.newPath(hex));
+    // }
+  };
+
+
+  return {
+    setWeight: setWeight,
+    renderColorTable: renderColorTable,
+    newColor: newColor,
+    sort: sortColorTable
+  };
+});
